9c872892778504dbbc007a62efc90d876249693f
tanim006 <tanimahamed680@gmail.com>
2024-08-08 02:59:31 +0600

    first commit

---
+++ scripts/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['DM Sans', 'sans-serif'],
+          jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
